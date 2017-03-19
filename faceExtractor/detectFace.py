3ec4d888b212782488deaa096f2d9e2f6f88e362
Frank Portman <fportman@uber.com>
2017-03-19 15:24:39 -0700

    tweak params to minimize necks

--- faceExtractor/detectFace.py
+++ faceExtractor/detectFace.py
@@ -17,12 +17,12 @@ face_cascade=cv2.CascadeClassifier(cascade)
 image=cv2.imread(image_path)
 image_grey=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(image_grey,scaleFactor=1.16,minNeighbors=5,minSize=(25,25),flags=0)
+faces = face_cascade.detectMultiScale(image_grey,scaleFactor=1.1,minNeighbors=5,minSize=(130,130),flags=0)
 
 for x,y,w,h in faces:
     sub_img=image[y-10:y+h+10,x-10:x+w+10]
-    os.chdir("../img/Extracted")
-    cv2.imwrite(os.path.basename(image_path)+"-extracted.jpg",sub_img)
+    os.chdir("../imgs/Extracted")
+    cv2.imwrite(os.path.basename(image_path)+"-extracted.png",sub_img)
     os.chdir("../")
 
 
