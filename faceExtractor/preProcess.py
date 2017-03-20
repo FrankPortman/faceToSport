0e0cfa28a942705e03550be9c9f3decf7c5e1658
Frank Portman <fportman@uber.com>
2017-03-20 12:10:49 -0700

    process all NFL, add safety check, add average face

--- faceExtractor/preProcess.py
+++ faceExtractor/preProcess.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 images = []
 
 for file in os.listdir('../imgs/Extracted/'):
-    images.append(cv2.imread(os.path.join('../imgs/Extracted/', file), 0))
+    img = cv2.imread(os.path.join('../imgs/Extracted', file), 0)
+    if img is not None:
+        images.append(img)
     
 
 images = [cv2.resize(i, (200, 200)) for i in images]
@@ -14,3 +16,22 @@ all([len(i) == (200 * 200) for i in imagesP])
 
 plt.imshow(images[0], cmap='gray')
 plt.imshow(images[84], cmap='gray')
+plt.imshow(images[1024], cmap='gray')
+
+kek = sum(images)
+kek = kek / len(images)
+
+plt.imshow(kek, cmap='gray')
+
+kek = np.ndarray(images)
+kek = kek.astype("double")
+
+kek + kek
+
+images[0].dtype
+
+kek = [i.astype("double") for i in images]
+kek = sum(kek)
+kek = kek / len(images)
+
+plt.imshow(kek, cmap='gray')
